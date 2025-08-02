c7ba96488d16c0b5b35e128435be4b24a8b130d5
rhk998 <130037816+rhk998@users.noreply.github.com>
2025-08-02 15:11:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes.forEach((code,idx)=>{
+	code.addEventListener("keydown", (e)=>{
+		const keyPressed = e.key;
+		if(keyPressed <= "9" && keyPressed >= "0"){
+			if(codes.length - 1 > idx){
+				setTimeout(()=>codes[idx+1].focus(),10);
+			}
+		}
+		else if(keyPressed === "Backspace"){
+				if(idx > 0){
+					setTimeout(()=>codes[idx-1].focus(),10);
+				}
+		}
+	})
+})
